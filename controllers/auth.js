cdfc8fc173573d63552e7a2685bec7c0a465c921
Furkan Özay <furkann.ozay@hotmail.com>
2024-01-22 23:35:11 +0300

    User register api oluşturuldu ve customError da iyileştirmeler yapıldı

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,14 +3,15 @@ const CustomError = require('../helpers/error/customError');
 const asyncErrorWrapper = require('express-async-handler');
 const register = asyncErrorWrapper(async (req, res, next) => {
 
-    console.log(req.body);
-    // const user = await User.create({
-    //     firstName,
-    //     lastName,
-    //     email,
-    //     username,
-    //     password
-    // });
+    const { firstName, lastName, username , email, password, role } = req.body;
+    const user = await User.create({
+        firstName,
+        lastName,
+        username,
+        email,
+        password,
+        role
+    });
     res.status(200).json({
         success: true,
         data: user
--- middlewares/errors/customError.js
+++ middlewares/errors/customError.js
@@ -9,6 +9,9 @@ const customErrorHandler = (err, req, res, next) => {
     if(err.name === "SyntaxError"){
         customError = new CustomError("Unexpected Syntax", 400);
     }
+    if(err.code === 11000){
+        customError = new CustomError("Böyle bir kayıt zaten var!", 400);
+    }
     res.status(customError.status || 500).json({
         success: false,
         message: customError.message
