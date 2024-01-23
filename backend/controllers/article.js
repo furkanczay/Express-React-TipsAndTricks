5f1b06cb531421085057347859ef58b8955b4213
Furkan Özay <furkann.ozay@hotmail.com>
2024-01-23 14:59:57 +0300

    Tüm Articleları getirme api route hazırlandı

--- backend/controllers/article.js
+++ backend/controllers/article.js
@@ -2,11 +2,13 @@ const Article = require("../models/Article");
 const CustomError = require('../helpers/error/customError');
 const asyncErrorWrapper = require('express-async-handler');
 
-const getAllArticles = (req, res, next) => {
-    res.status(200).json({
-        success: true
+const getAllArticles = asyncErrorWrapper(async (req,res,next) => {
+    const articles = await Article.find();
+    return res.status(200).json({
+        success: true,
+        data: articles
     })
-};
+})
 
 const newArticle = asyncErrorWrapper(async (req,res,next) => {
     const information = req.body;
